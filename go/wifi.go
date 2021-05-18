83a6e2b0dee05ad54bb69260e7b7c64e6fa1a426
Sergi Rene <sergi@datahop.network>
2021-05-18 12:41:27 +0200

    testing

--- go/wifi.go
+++ go/wifi.go
@@ -9,13 +9,10 @@ var (
 )
 
 
-// Hook is used by clients
 type WifiHotspot interface {
 	Start() //(string, string)
 	Stop()
 }
-
-type wifiHotspotNotifier struct{}
 
 type WifiHotspotNotifier interface {
 	OnSuccess()
@@ -27,32 +24,11 @@ type WifiHotspotNotifier interface {
 }
 
 
-func (hs *wifiHotspotNotifier) OnSuccess(){
-	log.Debug("Network up")
-}
-
-func (hs *wifiHotspotNotifier) OnFailure(code int){
-	log.Debug("hotspot failure ",code)
-}
-
-func (hs *wifiHotspotNotifier) StopOnSuccess(){
-	log.Debug("StopOnSuccess")
-}
-
-func (hs *wifiHotspotNotifier) StopOnFailure(code int){
-	log.Debug("StopOnFailure ",code)
-}
-
-func (hs *wifiHotspotNotifier) 	NetworkInfo(network string, password string) {
-	log.Debug("hotspot info ", network, password)
-}
-
 // Hook is used by clients
 type WifiConnection interface {
 	Connect(network string, pass string,ip string)
 	Disconnect()
 }
-type wifiConnectionNotifier struct{}
 
 type WifiConnectionNotifier interface {
 	OnSuccess()
@@ -61,15 +37,3 @@ type WifiConnectionNotifier interface {
 }
 
 
-func (hs *wifiConnectionNotifier) OnSuccess(){
-	log.Debug("Connection success")
-}
-
-func (hs *wifiConnectionNotifier) OnFailure(code int){
-	log.Debug("Connection failure ",code)
-}
-
-func (hs *wifiConnectionNotifier) OnDisconnect(){
-	log.Debug("OnDisconnect")
-
-}
